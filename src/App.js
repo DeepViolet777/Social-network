ccf2aee6bd40f20bf9e622d52e375aa8146cc3d0
Anna Malorodova <angervenus6@gmail.com>
2021-03-06 02:25:37 +0300

    Inicial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import cat from './assets/cat.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+      <header className="header">
+      <img alt="Logo" width='70px' height="80px" src={cat}/>
+      </header>
+      <nav className="navbar">
+        <div>
+        <a href="#">
+          Profile
+        </a>
+        </div>
+         <div>
+        <a href="#">
+          Messages
+        </a>
+        </div>
+         <div>
+        <a href="#">
+          News
+        </a>
+        </div>
+         <div>
+        <a href="#">
+          Music
+        </a>
+        </div>
+        <div>
+        <a href="#">
+          Settings
+        </a>
+        </div>
+      </nav>
+      <div className="content">Main content</div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
